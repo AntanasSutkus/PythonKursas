8799ea72e7cc967963dd15c7c7063385c048162f
Antanas Sutkus <sutkusantanas@gmail.com>
2024-03-27 21:23:11 +0200

    8 pamoka

---
+++ iki 03-26/8 pamoka.03-26.py
@@ -0,0 +1,16 @@
+i = 0
+while i < 10:
+  print(i)git
+  i += 1
+
+"""
+while True:
+    user_input = input("Enter your name: ")
+    if len(user_input) != 0:
+        break
+print(f"You entered {user_input }")
+"""
+
+names = ["Albert", "Tom", "Leonardo"]
+for name in names:
+    print(f"Greetings, {name}")
