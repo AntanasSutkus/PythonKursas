7d2cc8a1bcfdd1701f634e2baae4b481f1f5e19d
Antanas Sutkus <sutkusantanas@gmail.com>
2024-04-10 21:28:50 +0300

    04-10 Destytojo uzdaviniai

---
+++ funkcijos/destytojo_uzd_04-10.py
@@ -0,0 +1,9 @@
+
+
+"""1 pratimas: Parašykite Python programą, kad surastumėte dažniausiai pasitaikančius elementus ir jų skaičių nurodytame tekste.
+Originali eilutė: lkseropewdssafsdfafkpwe
+Dažniausiai trys minėtos eilutės simboliai [('s', 4), ('e', 3), ('f', 3)]"""
+
+from collections import Counter
+data = "lkseropewdssafsdfafkpwe"
+
